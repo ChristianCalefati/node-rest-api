995a3f94abe20f8918a8f6ed4713a2ad7209a436
Christian Calefati <c.calefati@gmail.com>
2024-12-29 12:09:16 +0100

    - populate-db.ts file completed with repo logic

--- src/model/populate-db.ts
+++ src/model/populate-db.ts
@@ -10,10 +10,41 @@ if (result.error) {
 import "reflect-metadata";
 import { AppDataSource } from "../data-source";
 import { COURSES } from "./data-db";
+import { DeepPartial } from "typeorm";
+import { Course } from "./course";
+import { Lesson } from "./lesson";
 
 async function populateDb() {
   await AppDataSource.initialize();
   console.log("Database connection ready");
+
+  const courses = Object.values(COURSES) as DeepPartial<Course>[];
+
+  const courseRepository = AppDataSource.getRepository(Course);
+  const lessonRepository = AppDataSource.getRepository(Lesson);
+
+  for (let courseData of courses) {
+    //Store courses data
+    console.log(`Inserting course  ${courseData.title}`);
+    const course = courseRepository.create(courseData);
+    await courseRepository.save(course);
+
+    //Store lessons data and link it to the course
+    for (let lessonData of course.lessons) {
+      console.log(
+        `Inserting lesson ${lessonData.seqNo} for course ${course.title}`
+      );
+      const lesson = lessonRepository.create(lessonData);
+      lesson.course = course;
+      await lessonRepository.save(lesson);
+    }
+
+    const totalCourses = await courseRepository.count();
+    const totalLessons = await lessonRepository.count();
+
+    console.log(`Total courses stored: ${totalCourses}`);
+    console.log(`Total lessons stored: ${totalLessons}`);
+  }
 }
 
 populateDb()
